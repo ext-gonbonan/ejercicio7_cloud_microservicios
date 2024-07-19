7796095fd4a48c34a60ec6e2d87b4d5f24f82279
ext-gonbonan <antonio.gonzalez@altia.es>
2024-07-19 19:30:35 +0100

    Actualiza

--- ejercicio7_cloud_microservicio_hotel/src/main/java/com/hotel/inicio/Application.java
+++ ejercicio7_cloud_microservicio_hotel/src/main/java/com/hotel/inicio/Application.java
@@ -37,7 +37,7 @@ public class Application {
                     Hotel.builder().nombre("Hotel Riu Palace Tenerife").categoria("4 estrellas").precio(200.00).disponible(true).build(),
                     Hotel.builder().nombre("Lopesan Baobab Resort").categoria("5 estrellas").precio(270.00).disponible(true).build(),
                     Hotel.builder().nombre("Hotel Jardines de Nivaria").categoria("5 estrellas").precio(320.00).disponible(true).build(),
-                    Hotel.builder().nombre("Hotel Tigaiga").categoria("4 estrellas").precio(180.00).disponible(true).build(),
+                    Hotel.builder().nombre("Hotel Dreams Playa Blanca").categoria("4 estrellas").precio(180.00).disponible(true).build(),
                     Hotel.builder().nombre("Hotel Suite Villa María").categoria("5 estrellas").precio(290.00).disponible(true).build(),
                     Hotel.builder().nombre("Hotel Las Madrigueras Golf Resort & Spa").categoria("5 estrellas").precio(310.00).disponible(true).build(),
                     Hotel.builder().nombre("Hotel Taburiente").categoria("4 estrellas").precio(150.00).disponible(true).build(),
@@ -48,7 +48,7 @@ public class Application {
                     Hotel.builder().nombre("Hotel Parador de El Hierro").categoria("3 estrellas").precio(150.00).disponible(true).build(),
                     Hotel.builder().nombre("Hotel Parador de Las Cañadas del Teide").categoria("3 estrellas").precio(170.00).disponible(true).build(),
                     Hotel.builder().nombre("Iberostar Grand El Mirador").categoria("5 estrellas").precio(330.00).disponible(true).build(),
-                    Hotel.builder().nombre("Red Level at Gran Meliá Palacio de Isora").categoria("5 estrellas").precio(340.00).disponible(true).build(),
+                    Hotel.builder().nombre("Hotel Gran Meliá Palacio de Isora").categoria("5 estrellas").precio(340.00).disponible(true).build(),
                     Hotel.builder().nombre("Hotel Botanico y Oriental Spa Garden").categoria("5 estrellas").precio(350.00).disponible(true).build(),
                     Hotel.builder().nombre("Gran Hotel Atlantis Bahía Real").categoria("5 estrellas").precio(360.00).disponible(true).build()
                 ));
