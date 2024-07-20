c6df992e15b9ed1a80ae09a1a5f659d50baba224
ext-gonbonan <antonio.gonzalez@altia.es>
2024-07-20 13:35:11 +0100

    Actualizaciones

--- ejercicio7_cloud_microservicio_hotel/src/main/java/com/hotel/controller/HotelController.java
+++ ejercicio7_cloud_microservicio_hotel/src/main/java/com/hotel/controller/HotelController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.hotel.service.HotelService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 
+@CrossOrigin(origins = "*")  // Permitimos solicitudes desde cualquier origen
 @RestController
 @RequestMapping("/hoteles")
 public class HotelController {
